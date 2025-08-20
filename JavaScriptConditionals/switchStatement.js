2ecb37d08e25d288350cc329c22e5ee17269e09f
Alice <alicewmaina021@gmail.com>
2025-08-20 14:37:50 +0300

    Add JavaScript Conditional statements

---
+++ JavaScriptConditionals/switchStatement.js
@@ -0,0 +1,13 @@
+const food = 'salad';
+
+switch (food) {
+  case 'oyster':
+    console.log('The taste of the sea');
+    break;
+  case 'pizza':
+    console.log('A delicious pie');
+    break;
+  default:
+    console.log('Enjoy your meal');
+}
+// Output: Enjoy your meal
---
+++ JavaScriptConditionals/tenaryOperator.js
@@ -0,0 +1,6 @@
+var x=1;
+
+// => true
+result = (x == 1) ? true : false;
+
+console.log(result); // true
